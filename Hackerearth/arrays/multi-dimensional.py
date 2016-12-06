e5bd5a0b28ca350a5283a3c8c4d829f686d6f48b
Rijumone Choudhuri <rijumone.choudhuri@mantralabsglobal.com>
2016-12-06 22:46:51 +0530

    foo

---
+++ Hackerearth/arrays/multi-dimensional.py
@@ -0,0 +1,22 @@
+'''
+# Read input from stdin and provide input before running code
+
+name = raw_input('What is your name?\n')
+print 'Hi, %s.' % name
+'''
+sRowsCols = raw_input('rows x columns?\n')
+
+sRowsCols = sRowsCols.split()
+rows  = sRowsCols[0];
+cols  = sRowsCols[1];
+
+print rows
+print cols
+
+
+for arrElement in rows : {
+  arr = raw_input('input 1st array\n')
+  arr1[arrElement] = arr.split()  
+}
+print arr1;
+
