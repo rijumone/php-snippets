58aac2de5b0d92fccb00c7b992ba8be76d7b611d
Rijumone Choudhuri <rijumone.choudhuri@mantralabsglobal.com>
2017-03-06 18:02:27 +0530

    check logic

--- Hackerearth/arrays/micro-and-array-update.php
+++ Hackerearth/arrays/micro-and-array-update.php
@@ -10,24 +10,31 @@ Sample input :
 3 2
 2 5 5
 
+Sample output :
+3
+0
+
 */
 
 $f = fopen('php://stdin', 'r');
 $temp = explode(" ", trim(fgets($f)));
 // print_r($temp);exit;
-$size = $temp[0];
-$nQueries = $temp[1];
+$testCases = $temp[0];
+for ($i = 0; $i < ($testCases); $i++) {
+	$arr = explode(" ", trim(fgets($f)));
+	$N = $arr[0];
+	$K = $arr[1];
 
-$arr1 = explode(" ", trim(fgets($f)));
-
-for ($i = 1; $i < ($nQueries+1); $i++) {
-    $input = explode(" ", trim(fgets($f)));
-    if($input[0]){
-      $arr1[($input[1]-1)] =  ($arr1[($input[1]-1)]) ? 0 : 1; 
-    }else{
-      echo ($arr1[($input[2]-1)]) ? "ODD" : "EVEN";
-      echo "\n";
-    }
+	$arr = explode(" ", trim(fgets($f)));
+	
+	$largestDiff = 0;
+	
+	for ($j = 0; $j < $N; $j++) {
+		if(($arr[$j]-$K)>$largestDiff){
+			$largestDiff = $arr[$j]-$K;
+		}    
+	}
+	echo ($largestDiff) ? $largestDiff : "0";echo "\n";
 }
 // print_r($arr1);exit;
 fclose($f);
