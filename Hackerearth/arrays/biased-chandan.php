0a8161badd8c5867650be466686d64dd822ebe6c
Riju <mailmeonriju@gmail.com>
2017-03-13 14:03:41 +0530

    finally solved

--- Hackerearth/arrays/biased-chandan.php
+++ Hackerearth/arrays/biased-chandan.php
@@ -1,6 +1,7 @@
 
 <?php
 
+error_reporting(0);
 /*
 
   Sample input :
@@ -15,56 +16,26 @@
   2
 
 
-$temp = array(3, 0, 8, 0, 0, 4);
-$last = 0;
-
-  $temp = array();
-
-  for ($i = 0; $i < $N; $i++) {
-  $temp[] = trim(fgets($f));
-  }
-  // $N=6;
-  $sum = 0;
-  for ($i = 0; $i < $N; $i++) {
-  if ($temp[$i] == 0) {
-  if ($last >= 0) {
-  $sum -= $temp[$last];
-  unset($temp[$last]);
-  unset($temp[$i]);
-  $last--;
-  } else {
-  unset($temp[$i]);
-  $last = $temp[$i + 1];
-  }
-  } else {
-  $sum += $temp[$i];
-  $last = $i;
-  }
-  }
-
-  echo $sum;
-  exit;
  */
-
-// unset($rating[1504]);
-// print_r($rating);
-// $f = fopen('php://stdin', 'r');
-$f = fopen('biased-chandan.txt', 'r');
+$f = fopen('php://stdin', 'r');
+// $f = fopen('biased-chandan.txt', 'r');
 
 $N = trim(fgets($f));
-if(!$N){trim(fgets($f));}
+if (!$N) {
+    trim(fgets($f));
+}
 $sum = 0;
 $rating = array();
 
 for ($i = 0; $i < $N; $i++) {
     $rating[] = trim(fgets($f));
 }
+//print_r($rating);exit;
 $zeroKeys = array();
 $lastZero = false;
 for ($i = 0; $i < $N; $i++) {
 
     if (!$rating[$i]) {
-      // unset($rating[$i]);
         if (count($zeroKeys)) {
             if (!$lastZero) {
                 $zeroKeys[$i] = 1;
@@ -78,42 +49,34 @@ for ($i = 0; $i < $N; $i++) {
         } else {
             $zeroKeys[$i] = 1;
         }
-        // $zeroKeys[$i] = ($lastZero) ? ($zeroKeys[$i] + 1) : 1;
         $lastZero = true;
     } else {
         $lastZero = false;
     }
 }
-$negativeSum = 0;
 // print_r($zeroKeys);
 foreach ($zeroKeys as $key => $val) {
     for ($i = 1; $i <= $val; $i++) {
-      // echo '$key: '. ($key - $i) . "\n";
-        $negativeSum += $rating[$key - $i];
+        // echo '$key: '. ($key - $i) . "\n";
+        // echo ($key - $i) . " : " . $rating[$key - $i]."\n";
+
+        if (!$rating[$key - $i]) {
+            $delKey = $key - $i - 1;
+            while (!$rating[$delKey]) {
+                $delKey--;
+            }
+            // echo $delKey . " : " . $rating[$delKey]."\n";
+            unset($rating[$delKey]);
+        }
         unset($rating[$key - $i]);
         // echo $rating[$key - $i]."\n";
     }
 }
 
-// foreach ($rating as $key => $val) {
-//     if (!$val) {
-//         // unset($rating[$key]);
-//         $delKey = $key - 1;
-//         // echo "========".$rating[$delKey]."========";exit;
-//         echo "========".$delKey."========\n";
-//        /* while (!$rating[$delKey]) {
-//             unset($rating[$delKey]);
-//             $delKey = $key -1;
-//         }*/
-//     }
-// }
-// print_r($rating);
-foreach($rating as $key=>$val){
+foreach ($rating as $key => $val) {
     $sum += $val;
-    // echo '$sum: '. $sum . "\n";
 }
 
-// echo $sum - $negativeSum;
 echo $sum;
 fclose($f);
 ?>
