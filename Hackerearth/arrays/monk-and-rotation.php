7dfa45ec8765635f91ac6ded5405ca970fa66dd1
Rijumone Choudhuri <rijumone.choudhuri@mantralabsglobal.com>
2017-03-20 12:22:09 +0530

    rotation

--- Hackerearth/arrays/monk-and-rotation.php
+++ Hackerearth/arrays/monk-and-rotation.php
@@ -17,10 +17,11 @@
 
  */
 // $f = fopen('php://stdin', 'r');
-$f = fopen('monk-and-rotation.txt', 'r');
+$f = fopen('monk-and-rotation.txt.bkp', 'r');
+// $f = fopen('monk-and-rotation.txt', 'r');
 
 $T = trim(fgets($f));
-
+// echo $T;exit;
 for ($k = 0; $k < $T; $k++) {
 
     $temp = explode(" ", fgets($f));
@@ -28,23 +29,35 @@ for ($k = 0; $k < $T; $k++) {
     
     // echo "\n";
     $nRotations = $temp[1];
+    // echo $nRotations."\n";
+    $nRotations = $nRotations % $N;
+    
+    // echo $N."\n";
+    // echo $nRotations;exit;
     // echo "\n";
     $arr = explode(" ", trim(fgets($f)));
     // print_r($arr);
     $arr2 = array();
-    for ($i = 0; $i < $nRotations; $i++) {
+    // for ($i = 0; $i < $nRotations; $i++) {
         for ($j = 0; $j < $N; $j++) {
-            if (!$j) {
+            /*if (!$j) {
                 $arr2[$j] = $arr[$N - 1];
             } else {
                 $arr2[$j] = $arr[$j - 1];
-            }
+            }*/
+            // echo $j;
+            // echo "----------\n". ($j + 1 + $nRotations) . "\n";
+            // echo ($j + 1 + ($nRotations - $N)) . "\n---------\n";
+            $changeVal = (isset($arr[$j +1+ $nRotations])) ? $arr[$j +1+ $nRotations] : $arr[$j +1+ ($nRotations - $N)];
+          $arr2[$j] = $changeVal;
+
         }
         $arr = $arr2;
-    }
+    // }
     for ($i = 0; $i < $N; $i++) {
         echo $arr[$i] . " ";
     }
+    // echo $arr[0];
     echo "\n";
 }
 
