35ddce4854d95849cbd7c8dcd6a24157c4c18bd3
Riju <mailmeonriju@gmail.com>
2017-04-19 00:52:17 +0530

    idk

--- test_curl.php
+++ test_curl.php
@@ -2,19 +2,23 @@
 
 //echo "test curl"; 
 
-
-$url = "http://localhost/cms/public/en/list-testimonials-home";
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
+$url = "http://icanhazip.com";
 
 
+for(;;){
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POST, true);
 
-// echo phpinfo();
-$contents = curl_exec($ch);
-echo $contents;
-curl_close($ch);
+
+
+	// echo phpinfo();
+	$contents = curl_exec($ch);
+	echo $contents;
+	curl_close($ch);
+}
+
 
 
 ?>
