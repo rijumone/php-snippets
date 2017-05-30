d9a0bb0c22890c49d0b3bf21d3b3d2193dc0e461
Riju <mailmeonriju@gmail.com>
2017-05-30 08:49:49 +0530

    removed competetive coding to new repo

--- Hackerearth/basic_programming/i-o/basics/factorial.py
+++
@@ -1,9 +0,0 @@
-def factorial(k):
-	if k == 1 :
-		return 1
-	else : 
-		return (k*factorial(k-1))
-
-n = int(raw_input())
-
-print(factorial(n))	
--- Hackerearth/basic_programming/i-o/basics/life-universe-everything.py
+++
@@ -1,8 +0,0 @@
-bool42found = False;
-
-while not bool42found :
-	integer = raw_input()
-	if(int(integer) == 42) :
-		bool42found = True
-	else :
-		print(integer)
