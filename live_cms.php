cb1eeb927e69bc8c8a4450bd74a5fd433d360efe
Rijumone Choudhuri <rijumone.choudhuri@mantralabsglobal.com>
2017-06-05 11:23:35 +0530

    live preview init comm

---
+++ live_cms.php
@@ -0,0 +1,10 @@
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<style type="text/css">
+	div: {
+		border: 2px red solid;
+	}
+</style>
+<div class="container">
+	
+</div>
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
