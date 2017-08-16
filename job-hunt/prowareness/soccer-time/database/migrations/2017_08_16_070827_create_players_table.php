15daa40d58ad305fbfce577285c02575d1606fe6
Riju <mailmeonriju@gmail.com>
2017-08-16 12:42:21 +0530

    soccer-time initial commit

---
+++ job-hunt/prowareness/soccer-time/database/migrations/2017_08_16_070827_create_players_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('firstName');
+            $table->string('lastName');
+
+            $table->timestamps();
+
+
+            $table->foreign('app_id')
+                ->references('id')
+                ->on('apps')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('players');
+    }
+}
